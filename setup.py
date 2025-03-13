4acf58ab9d8b4a9fd587b8a4dbbf105ccbda6b4e
SLJC <sljc@novonordisk.com>
2025-03-13 10:21:03 +0000

    Update dependencies and version; add support for typed_llm_images

--- setup.py
+++ setup.py
@@ -4,16 +4,19 @@ PACKAGE_NAME = "promptflow_typed_llm"
 
 setup(
     name=PACKAGE_NAME,
-    version="0.0.9",
+    version="0.1.0",
     description="Package for Promptflow tool that sends typed LLM requests",
     packages=find_packages(),
     entry_points={
-        "package_tools": ["typed_llm = typed_llm.tools.utils:list_package_tools"],
+        "package_tools": [
+            "typed_llm = typed_llm.tools.utils:list_package_tools",
+            "typed_llm_images = typed_llm.tools.utils:list_package_tools"
+        ],
     },
     include_package_data=True,   # This line tells setuptools to include files from MANIFEST.in
     install_requires=[
-        'promptflow>=1.15.0',
-        'promptflow[azure]>=1.15.0',
+        'promptflow>=1.17.0',
+        'promptflow[azure]>=1.17.0',
         'promptflow-tools>=1.4.0',
     ],
     classifiers = [
